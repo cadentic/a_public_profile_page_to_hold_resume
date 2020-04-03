1836207138c53ab8ec34ed475cd05897c70b57c2
lahiru94 <lahirud.14@cse.mrt.ac.lk>
2020-04-03 21:21:55 +0530

    chat implemetation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled, { css } from 'styled-components';
 import { StylesProvider } from '@material-ui/core/styles';
 import Navbar from './components/Navbar/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header1 from './components/Header/Header1';
+import ChatIcon from '@material-ui/icons/ChatBubbleOutline';
+import ChatView from './components/Chat/Chat'
 
 // Styles
 const Wrapper = styled.div`
@@ -23,14 +25,33 @@ const Wrapper = styled.div`
       background: #20242C;
     `}
   `;
-  const SideBar = styled.div`
+const SideBar = styled.div`
     width: 19px;
     background: #4169E1;
     height: 100%;
   `;
+const ChatButton = styled.button`
+    position:fixed; 
+    right:10px; 
+    bottom:10px;
+    border-radius: 60px;
+    background-color: red;
+    width:70px;
+    height:65px;
+  `
+const ChatViewWrapper = styled.div`
+    position:fixed; 
+    right:20px; 
+    bottom:80px;
+    ${props => props.hidden && css`
+      visibility:hidden
+    `}
+  `
 
 // Render
 function App(props) {
+
+  const [isChatVisible, setIsChatVisible ] = useState(false)
 
   return (
     <StylesProvider injectFirst>
@@ -38,9 +59,15 @@ function App(props) {
       <Wrapper bodyOuter>
         <SideBar />
         <Wrapper bodyInner>
-          <Header1/>
+          <Header1 />
         </Wrapper>
         <SideBar />
+        <ChatViewWrapper hidden={!isChatVisible}>
+          <ChatView />
+        </ChatViewWrapper>
+        <ChatButton onClick={()=>{setIsChatVisible(!isChatVisible)}}>
+          <ChatIcon fontSize='large' outlined style={{ color: 'white', marginLeft: '1px' }} />
+        </ChatButton>
       </Wrapper>
     </StylesProvider>
   );
---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,128 @@
+import React, {useState} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import styled, { css } from 'styled-components';
+import CallIcon from '@material-ui/icons/Call';
+import VideoCamIcon from '@material-ui/icons/Videocam';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '300px',
+        backgroundColor: 'rgb(50, 50, 51)',
+        color:'white',
+    },
+    media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+    },
+    avatar: {
+        backgroundColor: red[500],
+    },
+    cardContent: {
+        height:'300px',
+        overflowY:'auto'
+    },
+    cardHeader: {
+        backgroundColor:'black'
+    },
+    messageTypography:{
+        backgroundColor: 'grey',
+        borderRadius:'3px',
+        padding:'2px',
+        maxWidth:'200px'
+    }
+}));
+
+const ChatInput = styled.input`
+    width:100%;
+    background-color: black;
+    color:white;
+    border:none;
+    padding-left:20px;
+    border-radius:10px;
+`
+
+const MessageComponent = styled.div`
+    width:100%;
+    margin-top:8px;
+    display:flex;
+    ${props => props.sent && css`   
+    flex-direction: row-reverse;
+    `}
+`
+
+export default function Chat() {
+    const classes = useStyles();
+
+    const [messages, setMessages] = useState([
+        {text:'Hi how are you', sent:true},
+        {text:'Im fine, Thank you', sent:false}
+    ])
+
+    const [currMessage, setCurrMessage] = useState('')
+
+    const sendMessage = (message) => {
+        let newMessageArray = messages.slice()
+        newMessageArray.push({
+            text:message,
+            sent:true
+        })
+        setMessages(newMessageArray)
+        //AXIOS call
+    }
+
+    const handleOnEnter = (e) => {
+        if(e.keyCode===13){
+            sendMessage(currMessage)
+            setCurrMessage('')
+        }
+    }
+
+    return (
+        <Card className={classes.root}>
+            <CardHeader
+                className={classes.cardHeader}
+                avatar={
+                    <Avatar aria-label="recipe" className={classes.avatar}>
+                        J
+                    </Avatar>
+                }
+                action={
+                    <>
+                    <VideoCamIcon style={{marginTop:'15px', marginRight:'5px'}}/>
+                    <CallIcon style={{marginTop:'15px'}}/>
+                    <IconButton style={{color:'white'}} aria-label="settings">
+                        <MoreVertIcon />
+                    </IconButton>
+                    </>
+                }
+                title="John Doe"
+                subheader="online"
+            />
+            <CardContent className={classes.cardContent}>
+                {messages.map((message, index)=> 
+                (<MessageComponent key={index} sent={message.sent}>
+                    <Typography className={classes.messageTypography} variant="body2"  component="p">
+                        {message.text}
+                    </Typography>
+                </MessageComponent>))
+                }   
+            </CardContent>
+
+            <CardActions >
+                <ChatInput
+                    value={currMessage} 
+                    onChange={(e)=>{setCurrMessage(e.target.value)}}
+                    placeholder='type your message' onKeyUp={handleOnEnter} />
+            </CardActions>
+        </Card>
+    );
+}
